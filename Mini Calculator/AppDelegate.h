304f734e93dce299728b33d5ef627a0a198e0bf5
Student P_03 <studentp_03@student-p-03.local>
2016-08-03 14:33:03 +0530

    Initial Commit

---
+++ Mini Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mini Calculator
+//
+//  Created by Student P_03 on 03/08/16.
+//  Copyright © 2016 AkshayAwtade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mini Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mini Calculator
+//
+//  Created by Student P_03 on 03/08/16.
+//  Copyright © 2016 AkshayAwtade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
